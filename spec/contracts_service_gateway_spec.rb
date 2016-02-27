d9fa3d99a5f24528c7df92103044841dcd60d25a
Wang lei <wldandan@gmail.com>
2016-02-28 00:01:08 +0800

    Add logic to parse data

--- spec/contracts_service_gateway_spec.rb
+++ spec/contracts_service_gateway_spec.rb
@@ -10,7 +10,6 @@ describe ContractServiceGateway, pact: true do
   subject { ContractServiceGateway.new }
 
   describe 'get_products' do
-
     before do
       products_service.given('products exist').
           upon_receiving('a request for all products').
@@ -19,14 +18,32 @@ describe ContractServiceGateway, pact: true do
               status: 200,
               headers: { 'Content-Type' => 'application/hal+json' },
               body:    {
-                          products: [{id:1, name: 'Docker In Action', price: "59.00"},
-                                   {id:2, name: 'Agile In Action',  price: "55.00"}]}
+                        _links: {
+                            self: { href: "http://example.org/products" },
+                        },
+                        count: 2,
+                        _embedded: {
+                          products: [{id:1, name: 'Docker In Action', price: '59.00'},
+                                     {id:2, name: 'Agile In Action',  price: '55.00'}]}
+                        }
           )
     end
 
     it "returns products" do
-      expect(subject.get_products).to eq({"products"=>[{"id"=>1, "name"=>"Docker In Action", "price"=>"59.00"}, {"id"=>2, "name"=>"Agile In Action", "price"=>"55.00"}]})
+      expect(subject.get_products).to eq({"products"=>[{:title=>"1_Docker In Action_59.00"},
+                                                       {:title=>"2_Agile In Action_55.00"}]})
     end
   end
 
+  describe 'parse' do
+    it "parse the data" do
+      expect(subject.parse({"_links"=>{"self"=>{"href"=>"http://localhost:9292/products"}},
+                            "count"=>2,
+                            "_embedded"=>{"products"=>[
+                                {"id"=>1, "name"=>"Docker In Action", "price"=>"59.00"},
+                                {"id"=>2, "name"=>"Agile In Action", "price"=>"55.00"}]}}))
+      .to eq("products" => [{:title=>"1_Docker In Action_59.00"},
+                            {:title=>"2_Agile In Action_55.00"}])
+    end
+  end
 end
